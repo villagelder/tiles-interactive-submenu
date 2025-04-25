d90209439c9e7adf0fc35c83208d9158bb5640d6
Matthew Leopin <matthewleopin@gmail.com>
2025-04-25 11:52:35 -0400

    added submenu config

--- scripts/submenu.js
+++ scripts/submenu.js
@@ -1,3 +1,32 @@
+class TileSubmenuConfig extends FormApplication {
+  constructor(tile) {
+    super(tile);
+    this.tile = tile;
+  }
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      title: "Tile Submenu Configuration",
+      id: "tile-submenu-config",
+      template: "modules/tiles-interactive-submenu/templates/config.html",
+      width: 600,
+      height: "auto",
+    });
+  }
+
+  getData() {
+    return {
+      actions: this.tile.document.getFlag("tiles-interactive-submenu", "actions") || []
+    };
+  }
+
+  async _updateObject(event, formData) {
+    const data = expandObject(formData);
+    await this.tile.document.setFlag("tiles-interactive-submenu", "actions", data.actions);
+  }
+}
+
+
 Hooks.on("ready", () => {
     game.tileSubmenu = {
       showMenu: async (token) => {
